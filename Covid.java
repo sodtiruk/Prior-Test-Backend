a47670108505e20e91abb90f1acaadde8cdf10d3
sodtiruk <sodtiruk22@gmail.com>
2024-03-09 12:07:40 +0700

    first commit

---
+++ Covid.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Covid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String gender = scan.next();
+
+        
+    }
+}
